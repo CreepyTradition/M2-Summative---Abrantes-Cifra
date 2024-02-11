d6e29579958a708364c2bb0634645a39f4f19920
Joshua Emmanuel Cifra <jeCifra@mcm.edu.ph>
2024-02-11 19:48:28 +0800

    Box Sound Fixed
    Changed audio source  iteam prefab to itembox gameobject (MAINLOOP)

--- Assets/Scripts/BoxManager.cs
+++ Assets/Scripts/BoxManager.cs
@@ -6,6 +6,8 @@ public class BoxManager : MonoBehaviour
 {
     public GameObject itemBox;
     public float respawnDelay = 3f;
+    public AudioSource boxSoundSource;
+    public AudioClip boxSoundClip;
 
     private bool isBoxSpawned = false;
 
@@ -38,12 +40,22 @@ public class BoxManager : MonoBehaviour
         if (boxTrigger.gameObject.tag == "Player")
         {
             BoxDestroyed();
+            boxGetSound();
         }
     }
 
     public void BoxDestroyed()
     {
         isBoxSpawned = false;
+    }
+
+    void boxGetSound()
+    {
+        if (boxSoundSource != null && boxSoundClip != null)
+        {
+            boxSoundSource.clip = boxSoundClip;
+            boxSoundSource.Play();
+        }
     }
 
    
--- Assets/Scripts/ItemBoxController.cs
+++ Assets/Scripts/ItemBoxController.cs
@@ -7,32 +7,18 @@ public class ItemBoxController : MonoBehaviour
     private bool impact;
     public AudioSource boxSoundSource;
     public AudioClip boxSoundClip;
+    
 
     void OnTriggerEnter(Collider itemGet)
     {
         if(itemGet.gameObject.tag == "Player")
         {
             ItemBoxDestroy();
-            playBoxSound();
         }
     }
 
     void ItemBoxDestroy()
     {
         Destroy(gameObject);
-    }
-
-    void playBoxSound()
-    {
-        if (boxSoundClip != null)
-        {
-            boxSoundSource.clip = boxSoundClip;
-            if (!boxSoundSource.enabled)
-            {
-                boxSoundSource.enabled = true;
-            }
-            Debug.Log("Played");
-            boxSoundSource.Play();
-        }
     }
 }
